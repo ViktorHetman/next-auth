98f9d30e2dc3976e2db76bba07d318d79ecd399a
Viktor <vitya.getman.79@gmail.com>
2024-03-13 18:35:25 +0100

    creating user in the table

--- actions/register.ts
+++ actions/register.ts
@@ -1,8 +1,11 @@
 "use server";
 
 import * as zod from "zod";
+import bcrypt from "bcrypt";
 
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: zod.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -10,6 +13,25 @@ export const register = async (values: zod.infer<typeof RegisterSchema>) => {
   if (!validatedFields.success) {
     return { error: "Invalid fields!" };
   }
+
+  const { email, name, password } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  //TODO: Send verification token email
 
   return { success: "Account was created successfully!" };
 };
--- schemas/index.ts
+++ schemas/index.ts
@@ -1,7 +1,7 @@
 import * as zod from "zod";
 
 const passwordValidation = new RegExp(
-  /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])$/
+  /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$/
 );
 
 export const LoginSchema = zod.object({
@@ -21,11 +21,12 @@ export const RegisterSchema = zod.object({
     .email({ message: "Email is required" }),
   password: zod
     .string()
-    .min(6, { message: "Password should contain at least 6 characters" })
     .regex(passwordValidation, {
       message:
         "Password must contain at least one big character, one number and one symbol",
-    }),
+    })
+    .min(6, { message: "Password should contain at least 6 characters" }),
+
   name: zod.string().min(1, {
     message: "Name is required",
   }),
